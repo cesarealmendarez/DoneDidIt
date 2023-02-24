8b6a8e42fa1d68821f0dac03c85e2272eb706ad6
Cesar <cesarealmendarez@gmail.com>
2023-02-23 18:53:38 -0800

    ⚡✨ Git Init

---
+++ DoneDidIt/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DoneDidIt
+//
+//  Created by Cesar Almendarez on 2/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ DoneDidIt/DoneDidItApp.swift
@@ -0,0 +1,28 @@
+//
+//  DoneDidItApp.swift
+//  DoneDidIt
+//
+//  Created by Cesar Almendarez on 2/23/23.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+
+@main
+struct DoneDidItApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
